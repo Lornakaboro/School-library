76698a01ca915d4f9c39d51f36d7dfbb728fa798
Lornakaboro <lornakaboro@gmail.com>
2023-05-19 11:03:02 +0300

    Create association between rental and person

--- person.rb
+++ person.rb
@@ -3,16 +3,18 @@ require_relative 'trimmer_decorator'
 require_relative 'capitalize_decorator'
 
 class Person < Nameable
+
+  attr_reader :id
+  attr_accessor :name, :age, :books, :rentals
+  
   def initialize(age, name: 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   private
 
@@ -21,6 +23,11 @@ class Person < Nameable
 
     false
   end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
+  end
+
 
   public
 
